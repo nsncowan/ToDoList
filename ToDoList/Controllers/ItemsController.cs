ca75362db45fb903b0fa0828cf8569992a7cfece
Noah Cowan <nsncowan@gmail.com>
2023-02-22 18:37:26 -0800

    save end of week

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,18 +6,17 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
+/*     [HttpGet("/items/new")]
     public ActionResult New()
     {
       return View();
+    }
+ */
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
+    {
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
 
     [HttpPost("/items")]
@@ -27,19 +26,23 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
 
-    [HttpGet("/items/{id}")]
+/*     [HttpGet("/items/{id}")]
     public ActionResult Show(int id)
     {
       Item foundItem = Item.Find(id);
       return View(foundItem);
     }
-
+ */
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
   }
 }
